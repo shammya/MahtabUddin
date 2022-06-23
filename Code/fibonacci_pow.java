6f174019975cd0bc7f435735f5361f0429053b42
Shammya <46061332+shammya@users.noreply.github.com>
2022-06-23 22:17:17 +0600

    Update fibonacci_pow.java

--- Code/fibonacci_pow.java
+++ Code/fibonacci_pow.java
@@ -17,7 +17,7 @@ public class MyClass {
     
     public static int fib(int n){
         
-        if(n<=1)
+        if(n==1 || n==0)
             return 1;
         else
             return fib(n-1)+fib(n-2);
